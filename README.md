e1f9fd771aa74915ecf1ff1eb403f4dc6a42e137
sora <1105698754@qq.com>
2021-01-11 00:30:01 +0800

    add:初始提交，创建CMakeLists.txt

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# openglDemo
+# testDemo
 demo
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "buildConfig.h"
+
+using namespace std;
+
+int main()
+{
+    printf("currentVersion: %d.%d\n", VERSION_MAJOR, VERSION_MINOR);
+
+    puts("test");
+
+    return 0;
+}
+
